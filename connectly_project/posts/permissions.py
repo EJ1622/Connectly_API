32033b0110b516ad04e38d6e61b88517053cedd9
El Jean L. Rementas <rementasej2002@gmail.com>
2026-02-09 05:14:39 -0500

    API Security for Connectly

---
+++ connectly_project/posts/permissions.py
@@ -0,0 +1,14 @@
+from rest_framework import permissions
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        # Read permissions for any request
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        
+        # Write permissions only for owner
+        return obj.author == request.user
+
+class IsAdminUser(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user and request.user.is_staff
--- connectly_project/posts/views.py
+++ connectly_project/posts/views.py
@@ -1,33 +1,58 @@
 from rest_framework import generics, status
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.views import TokenObtainPairView
 from django.contrib.auth.models import User
 from .models import Post, Comment
 from .serializers import UserSerializer, PostSerializer, CommentSerializer
+from .permissions import IsOwnerOrReadOnly, IsAdminUser
+from rest_framework.response import Response
+from django.contrib.auth import get_user_model
 
-# User Views
-class UserListCreateView(generics.ListCreateAPIView):
+User = get_user_model()
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        # Add custom claims
+        token['username'] = user.username
+        token['user_id'] = user.id
+        return token
+    
+    def post(self, request, *args, **kwargs):
+        response = super().post(request, *args, **kwargs)
+        # Safe way to add user info
+        if request.user.is_authenticated:
+            response.data['user'] = {
+                'id': request.user.id,
+                'username': request.user.username
+            }
+        return response
+
+# Protected User Views
+class UserListView(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
 
-class UserDetailView(generics.RetrieveAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-# Post Views
+# Protected Post Views
 class PostListCreateView(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated]
 
-class PostDetailView(generics.RetrieveAPIView):
+class PostDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
 
-# Comment Views
+# Protected Comment Views  
 class CommentListCreateView(generics.ListCreateAPIView):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticated]
 
-class CommentDetailView(generics.RetrieveAPIView):
+class CommentDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
